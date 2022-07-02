d0afcc8cbf0ecc5de85b782ce37e9f8ebef2f860
mahdi parivash <mahdiparivash@protonmail.com>
2022-07-02 09:02:23 +0430

    Update App UI

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-    <BrowserRouter>
+  <React.StrictMode>
     <App />
-    </BrowserRouter>
- 
+  </React.StrictMode>
 );
-
-
